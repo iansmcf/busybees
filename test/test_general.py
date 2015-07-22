d002a6802ee4cf223c739a2ab42701c8184b6bec
Ian McFarlane <ianm@cybernetics.com>
2015-07-22 11:12:53 -0400

    Change tabs to spaces

--- test/test_general.py
+++ test/test_general.py
@@ -8,34 +8,32 @@ from busybees import hive
 import pash
 
 class ErrWorker(worker.Worker):
-	def work(self, command):
-		proc = pash.ShellProc()
-		proc.run(command)
-		return "Exit code: %s" % proc.get_val('exit_code')
+    def work(self, command):
+        proc = pash.ShellProc()
+        proc.run(command)
+        return "Exit code: %s" % proc.get_val('exit_code')
 
 def test_hive():
-	apiary = hive.Hive()
-	apiary.create_queen('A1')
+    apiary = hive.Hive()
+    apiary.create_queen('A1')
+    apiary.create_queen('A2')
+    apiary.start_queen('A1')
+    apiary.start_queen('A2')
 
-	apiary.create_queen('A2')
-
-	apiary.start_queen('A1')
-
-	apiary.start_queen('A2')
-
-	jobs = ["iscsiadm -m discovery -t st -p 192.168.88.110",
+    jobs = ["iscsiadm -m discovery -t st -p 192.168.88.110",
             "iscsiadm -m discovery -t st -p 192.168.90.110",
             "iscsiadm -m discovery -t st -p 192.168.88.110"]
-	apiary.instruct_queen('A1', jobs, ErrWorker)
+    apiary.instruct_queen('A1', jobs, ErrWorker)
 
-	apiary.kill_queen('A1')
+    apiary.kill_queen('A1')
 
-	time.sleep(3)
+    time.sleep(3)
 
-	this = apiary.die()
+    this = apiary.die()
 
-	for key in this.keys():
-		for i in this[key]:
-			print i
+    for key in this.keys():
+        for i in this[key]:
+            assert i == "Exit code: 6"
+            print i
 
-	sys.exit(0)
+    sys.exit(0)
